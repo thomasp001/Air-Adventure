faced3d837b58d1291d981c939d17c07f222520e
Shreya Tekumalla <Shreya@d-i184-52-244.staff.eduroam.uq.edu.au>
2016-07-30 14:49:54 +1000

    Added chart thingie

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
-    //alert("SCRIPT LOADED!")
+    var chart = new CanvasJS.Chart("myChart", {
+		title:{
+			text: "My First Chart in CanvasJS"              
+		},
+		data: [              
+		{
+			// Change type to "doughnut", "line", "splineArea", etc.
+			type: "pie",
+			dataPoints: [
+				{ label: "apple",  y: 10  },
+				{ label: "orange", y: 15  },
+				{ label: "banana", y: 25  },
+				{ label: "mango",  y: 30  },
+				{ label: "grape",  y: 28  }
+			]
+		}
+		]
+		});
+		
+		chart.render();
 
 });
