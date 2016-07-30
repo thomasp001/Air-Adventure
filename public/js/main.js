87b9d72d61681637e759e97ff7535ab8d488ff36
Thomas Payne <thomas.payne39@yahoo.com>
2016-07-30 17:50:58 +1000

    Maps test

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,12 @@ $(document).ready(function() {
 
 
 });
+
+
+function initMap() {
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
+      }
