08ff3acc836decdbed1840e06faec5dac166be3a
Thomas Payne <thomas.payne39@yahoo.com>
2016-07-30 18:08:20 +1000

    Hopefully have queensland

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 function initMap() {
         var mapDiv = document.getElementById('map');
         var map = new google.maps.Map(mapDiv, {
-            center: {lat: -20.9176, lng: -142.7028},
+            center: {lat: -20.9176, lng: 142.7028},
             zoom: 8
         });
       }
