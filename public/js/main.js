22d485e41c708d69d53e9457f86b962bc895b699
Shreya Tekumalla <Shreya@d-i184-52-244.staff.eduroam.uq.edu.au>
2016-07-30 14:55:44 +1000

    Changed the thing and the other thing

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
   // Place JavaScript code here...
     var chart = new CanvasJS.Chart("myChart", {
 		title:{
-			text: "My First Chart in CanvasJS"              
+			text: "Concentration of gases in the atmosphere"              
 		},
 		data: [              
 		{
 			// Change type to "doughnut", "line", "splineArea", etc.
-			type: "pie",
+			type: "bar",
 			dataPoints: [
-				{ label: "apple",  y: 10  },
-				{ label: "orange", y: 15  },
-				{ label: "banana", y: 25  },
+				{ label: "Carbon Dioxide",  y: 10  },
+				{ label: "Oxygen", y: 15  },
+				{ label: "Nitrogen", y: 25  },
 				{ label: "mango",  y: 30  },
 				{ label: "grape",  y: 28  }
 			]
