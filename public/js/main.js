6c464abbbd6b19f03f3f8600c95857277127f184
Thomas Payne <thomas.payne39@yahoo.com>
2016-07-31 10:00:21 +1000

    Changed name of counter var

--- public/js/main.js
+++ public/js/main.js
@@ -64,14 +64,14 @@ function initMap() {
 
 
 placePins = function(data) {
-    var counter = 0;
-    while (counter <= data.length) {
+    var myCounter = 0;
+    while (myCounter <= data.length) {
         var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(d[counter]["latitude"],d[counter]["longitude"]),
+          position: new google.maps.LatLng(d[myCounter]["latitude"],d[myCounter]["longitude"]),
           map: map,
-          title: d[counter]["name"]
+          title: d[myCounter]["name"]
         });
-    counter++;
+    myCounter++;
     }
 }
 
